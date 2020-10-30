0c4b800ca0f4fb14bb1e4220e995b191b8b1c740
Muzyar Rad <muzyar.rad@gmail.com>
2020-10-30 11:23:16 -0700

    html file created

--- inquiry.js
+++ inquiry.js
@@ -1,7 +1,11 @@
 const inquirer = require("inquirer");
 
 inquirer
-  .prompt([{ name: "name", message: "What is your name? " }])
-  .then((answers) => {
-    console.info(answers.name);
-  });
+  .prompt([
+    { name: "name", message: "What is your name? " },
+    { name: "location", message: "Where do you live?" },
+    { name: "bio", message: "Tell me about yourself:" },
+    { name: "linkedin", message: "Enter your Linkedin URL:" },
+    { name: "github", message: "Enter your GitHub URL:" },
+  ])
+  .then((answers) => {});
