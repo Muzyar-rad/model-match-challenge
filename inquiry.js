3eebeabee8e56cae712c7762094ae2c7ae24677d
Muzyar Rad <muzyar.rad@gmail.com>
2020-10-30 17:13:15 -0700

    added default values

--- inquiry.js
+++ inquiry.js
@@ -2,10 +2,10 @@ const inquirer = require("inquirer");
 
 inquirer
   .prompt([
-    { name: "name", message: "What is your name? " },
-    { name: "location", message: "Where do you live?" },
-    { name: "bio", message: "Tell me about yourself:" },
-    { name: "linkedin", message: "Enter your Linkedin URL:" },
-    { name: "github", message: "Enter your GitHub URL:" },
+    { name: "name", message: "What is your name? ", default: "N/A" },
+    { name: "location", message: "Where do you live?", default: "N/A" },
+    { name: "bio", message: "Tell me about yourself:", default: "N/A" },
+    { name: "linkedin", message: "Enter your Linkedin URL:", default: "N/A" },
+    { name: "github", message: "Enter your GitHub URL:", default: "N/A" },
   ])
   .then((answers) => {});
