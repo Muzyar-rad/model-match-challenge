055e8e088ddf2be0537c5b72cd7128b32201dd39
Muzyar Rad <muzyar.rad@gmail.com>
2020-10-30 17:47:18 -0700

    added the HTML file

--- inquiry.js
+++ inquiry.js
@@ -1,11 +1,144 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 inquirer
   .prompt([
     { name: "name", message: "What is your name? ", default: "N/A" },
-    { name: "location", message: "Where do you live?", default: "N/A" },
+    { name: "street", message: "What is your street address?", default: "N/A" },
+    { name: "city", message: "What city do you live at?", default: "N/A" },
+    { name: "state", message: "What state?", default: "N/A" },
+    { name: "zipcode", message: "What is your zipcode?", default: "N/A" },
     { name: "bio", message: "Tell me about yourself:", default: "N/A" },
     { name: "linkedin", message: "Enter your Linkedin URL:", default: "N/A" },
     { name: "github", message: "Enter your GitHub URL:", default: "N/A" },
   ])
-  .then((answers) => {});
+  .then((answers) => {
+    const {
+      name,
+      street,
+      city,
+      state,
+      zipcode,
+      bii,
+      linkedin,
+      github,
+    } = answers;
+    fs.appendFile(
+      "portfolio.html",
+      `<!DOCTYPE html>
+      <html>
+        <head>
+          <meta http-equiv="refresh" content="3" />
+          <link
+            rel="stylesheet"
+            href="./font-awesome-4.7.0/css/font-awesome.min.css"
+          />
+          <title>Software Engineer Portfolio</title>
+          <style>
+            div.container {
+              display: flex;
+              background-color: #000051;
+              color: #80d8ff;
+              border: 4px solid;
+              border-radius: 6px;
+              max-width: 100%;
+              height: 400px;
+            }
+            div.column1 {
+              flex: 50%;
+              padding: 20px;
+            }
+            div.column2 {
+              flex: 50%;
+              padding: 20px;
+              border-left: 4px solid;
+            }
+            div.flex {
+              display: flex;
+              flex-direction: column;
+            }
+            div.nameTitle {
+              display: flex;
+              flex-direction: column;
+              align-items: flex-start;
+            }
+            p.name {
+              font-size: 30px;
+              margin-bottom: -8px;
+            }
+      
+            p.title {
+              font-size: 15px;
+            }
+            div.center {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+            div.location {
+              display: flex;
+              flex-direction: row;
+              align-items: baseline;
+              font-size: 15px;
+            }
+            div.address {
+              display: flex;
+              flex-direction: column;
+              margin-left: 10px;
+            }
+            p.street,
+            p.city {
+              margin-bottom: -15px;
+            }
+            div.github,
+            div.linkedin {
+              display: flex;
+              flex-direction: row;
+              align-items: baseline;
+            }
+            p.gitUrl,
+            p.linkedinUrl {
+              margin-left: 10px;
+            }
+            p.bio {
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <div class="column1 flex">
+              <div class="nameTitle">
+                <p class="name">Muzyar Rad</p>
+                <p class="title">Software Engineer</p>
+              </div>
+              <div class="location">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <div class="address">
+                  <p class="street">3922 Acacia Street</p>
+                  <p class="city">Irvine, CA</p>
+                  <p class="zipcode">92606</p>
+                </div>
+              </div>
+              <div class="github">
+                <i class="fa fa-github" aria-hidden="true"></i>
+                <p class="gitUrl">https://github.com/Muzyar-rad</p>
+              </div>
+              <div class="linkedin">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                <p class="linkedinUrl">https://www.linkedin.com/in/muzyar-rad/</p>
+              </div>
+            </div>
+            <div class="column2 center">
+              <p class="bio">
+                I am Muzyar (Mazy) Rad. Full-stack web developer with BS in computer
+                software engineering who is passionate about coding, learning and
+                working with new technology and tools, solving problems and overcoming
+                challenges.
+              </p>
+            </div>
+          </div>
+        </body>
+      </html>
+      `
+    );
+  });
