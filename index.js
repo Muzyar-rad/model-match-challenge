c68c0b734100bbb228b92f8b99661826d5d95e8c
Muzyar Rad <muzyar.rad@gmail.com>
2020-10-31 20:03:11 -0700

    added the documentation to index file

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const asyncResult = require("./asyncPortfolio");
 const promiseResult = require("./promisePortfolio");
 
 const solution = async () => {
+  /*Put one of each in comment, asyncResult's asynchronous code is run with async/await
+   and exceptions are handled with try/catch and promiseResult's asynchronous code is 
+   run with Promise Objects*/
+
   // await asyncResult();
   promiseResult();
 };
